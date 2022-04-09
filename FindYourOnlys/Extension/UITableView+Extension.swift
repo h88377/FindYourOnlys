13712014efcb5ac5882a315701d4ead15df8a701
Wayne <h88377@gmail.com>
2022-04-09 17:25:35 +0800

    Design AdoptTableViewCell.

---
+++ FindYourOnlys/Extension/UITableView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UITableView+Extension.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/9.
+//
+
+import Foundation
---
+++ FindYourOnlys/Extension/UITableViewCell+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UITableViewCell+Extension.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/9.
+//
+
+import Foundation
