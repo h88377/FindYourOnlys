e4e0daf68d9ceda32d0445c20f1ff97c587013e2
Wayne <h88377@gmail.com>
2022-04-09 20:50:39 +0800

    Modify AdoptCell design and design FavoriteTableViewCell layout.

---
+++ FindYourOnlys/View/AdoptCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  AdoptTableViewCell.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/9.
+//
+
+import UIKit
+
+class AdoptCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var sexLabel: UILabel!
+    
+    @IBOutlet weak var varietyLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    
+}
---
+++ FindYourOnlys/View/FavoriteTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/9.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var photoImageView: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var sexLabel: UILabel!
+    
+    @IBOutlet weak var varietyLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
