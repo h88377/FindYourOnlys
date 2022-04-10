7a56c54a76fd755d7754fbe13bfbb580f2696a11
Wayne <h88377@gmail.com>
2022-04-10 16:21:59 +0800

    Load Favorite pet from local storage.

--- FindYourOnlys/Controller/AdoptFavoriteViewController.swift
+++ FindYourOnlys/Controller/AdoptFavoriteViewController.swift
@@ -8,10 +8,70 @@
 import UIKit
 
 class AdoptFavoriteViewController: UIViewController {
-
+    
+    let viewModel = AdoptFavoriteViewModel()
+    
+    @IBOutlet weak var tableView: UITableView! {
+        
+        didSet {
+            
+            tableView.dataSource = self
+            
+            tableView.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTableView()
+        
+        viewModel.fetchFavoritePet { error in
+            
+            if error != nil {
+                
+                print(error)
+            }
+        }
+        
+        viewModel.petViewModels.bind { [weak self] petViewModels in
+            
+            DispatchQueue.main.async {
+                
+                self?.tableView.reloadData()
+            }
+        }
+    }
+    
+    func setupTableView() {
+        
+        tableView.registerCellWithIdentifier(identifier: FavoriteTableViewCell.identifier)
+        
     }
     
 }
+
+// MARK: - UITableViewDataSource & Delegate
+extension AdoptFavoriteViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        viewModel.petViewModels.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteTableViewCell.identifier, for: indexPath) as? FavoriteTableViewCell
+                
+        else { return UITableViewCell() }
+        
+        let cellViewModel = viewModel.petViewModels.value[indexPath.item]
+        
+        cell.configureCell(with: cellViewModel)
+        
+        return cell
+    }
+    
+}
+
---
+++ FindYourOnlys/ViewModel/AdoptFavoriteViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  AdoptFavoriteViewModel.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/10.
+//
+
+import Foundation
+
+class AdoptFavoriteViewModel {
+    
+    let petViewModels = Box([PetViewModel]())
+    
+    func fetchFavoritePet(completion: (Error?) -> Void ) {
+        
+        StorageManager.shared.fetchPet { result in
+            
+            switch result {
+                
+            case .success(let lsPets):
+                
+                self.setPets(with: lsPets)
+                
+            case .failure(let error):
+                
+                completion(error)
+            }
+        }
+        
+    }
+    
+    private func convertLsPetsToPets(from lsPets: ([LSPet])) -> [Pet] {
+        
+        var pets = [Pet]()
+        
+        for lsPet in lsPets {
+            
+            let pet = Pet(
+                id: Int(lsPet.id), location: lsPet.location, kind: lsPet.kind,
+                sex: lsPet.sex, bodyType: lsPet.bodyType, color: lsPet.color,
+                age: lsPet.age, sterilization: lsPet.sterilization, bacterin: lsPet.bacterin,
+                foundPlace: lsPet.foundPlace, status: lsPet.status, remark: lsPet.remark,
+                openDate: lsPet.openDate, closedDate: lsPet.closedDate,
+                updatedDate: lsPet.updatedDate, createdDate: lsPet.createdDate,
+                photoURLString: lsPet.photoURLString, address: lsPet.address, telephone: lsPet.telephone,
+                variety: lsPet.variety
+            )
+            pets.append(pet)
+        }
+        
+        return pets
+    }
+    
+    private func convertLsPetsToViewModels(from lsPets: [LSPet]) -> [PetViewModel] {
+        
+        let pets = convertLsPetsToPets(from: lsPets)
+        
+        var viewModels = [PetViewModel]()
+        
+        for pet in pets {
+            
+            let viewModel = PetViewModel(model: pet)
+            
+            viewModels.append(viewModel)
+        }
+        
+        return viewModels
+    }
+    
+    private func setPets(with lsPets: [LSPet]) {
+        
+        petViewModels.value = convertLsPetsToViewModels(from: lsPets)
+    }
+    
+}
