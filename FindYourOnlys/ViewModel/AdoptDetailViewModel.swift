3ad7f366c9d8b4d1388d7b480fdba3a9ccc37be0
Wayne <h88377@gmail.com>
2022-04-12 11:04:08 +0800

    Install FirebaseFirestoreSwift and save/delete data in Firebase.

--- FindYourOnlys/ViewModel/AdoptDetailViewModel.swift
+++ FindYourOnlys/ViewModel/AdoptDetailViewModel.swift
@@ -16,11 +16,13 @@ class AdoptDetailViewModel {
         case remove = "Remove"
     }
     
-    var isLogin: Bool = false
+    var didLogin: Bool = false
     
     let adoptDetailDescription = AdoptDetailDescription.allCases
     
-    let favoritePetViewModels = Box([FavoritePetViewModel]())
+    let favoriteLSPetViewModels = Box([FavoriteLSPetViewModel]())
+    
+    let favoritePetViewModels = Box([PetViewModel]())
     
     var favoritePetsFromLS = [LSPet]()
     
@@ -58,6 +60,29 @@ class AdoptDetailViewModel {
         }
     }
     
+    // MARK: - Local Storage functions
+    func fetchFavoritePetFromLS(completion: (Error?) -> Void ) {
+        
+        StorageManager.shared.fetchPet { result in
+            
+            switch result {
+                
+            case .success(let lsPets):
+                
+                self.setPets(with: lsPets)
+                
+                self.favoritePetsFromLS = lsPets
+                
+                completion(nil)
+                
+            case .failure(let error):
+                
+                completion(error)
+            }
+        }
+        
+    }
+    
     func addToFavoriteInLS() {
         
         StorageManager.shared.savePetInFavorite(with: petViewModel.value)
@@ -66,7 +91,7 @@ class AdoptDetailViewModel {
     func removeFavoriteFromLS() {
         
         let removeId = petViewModel.value.pet.id
-        
+            
         for favoritePetFromLS in favoritePetsFromLS {
             
             if favoritePetFromLS.id == removeId {
@@ -78,31 +103,102 @@ class AdoptDetailViewModel {
         }
     }
     
+    // MARK: - Firebase functions
+    func fetchFavoriteFromFB(completion: @escaping (Error?) -> Void) {
+        
+        FavoritePetFirebaseManager.shared.fetchFavoritePets { result in
+            
+            switch result {
+                
+            case .success(let pets):
+                
+                self.setPets(pets)
+                
+                completion(nil)
+                
+            case .failure(let error):
+                
+                completion(error)
+            }
+        }
+    }
+    
+    func addToFavoriteInFB(completion: @escaping (Error?) -> Void) {
+        
+        FavoritePetFirebaseManager.shared.saveFavoritePet("123", with: petViewModel.value) { error in
+            
+            completion(error)
+        }
+    }
+    
+    func removeFavoriteFromFB() {
+        
+        FavoritePetFirebaseManager.shared.removeFavoritePet(with: petViewModel.value)
+    }
+    
+    
+    // MARK: - Common functions
+    // Use for AdoptDetailVC viewDidLoad
     func checkFavoriteButton(with favoriteButton: UIButton) {
         
         favoriteButton.setTitle(FavoriteType.add.rawValue, for: .normal)
         
-        for favoritePetViewModel in favoritePetViewModels.value {
+        if !didLogin {
             
-            if favoritePetViewModel.lsPet.id == petViewModel.value.pet.id {
+            for favoriteLSPetViewModel in favoriteLSPetViewModels.value {
                 
-                favoriteButton.setTitle(FavoriteType.remove.rawValue, for: .normal)
+                if favoriteLSPetViewModel.lsPet.id == petViewModel.value.pet.id {
+                    
+                    favoriteButton.setTitle(FavoriteType.remove.rawValue, for: .normal)
+                    
+                    break
+                }
+            }
+            
+        } else {
+            
+            for favoritePetViewModel in favoritePetViewModels.value {
                 
-                break
+                if favoritePetViewModel.pet.id == petViewModel.value.pet.id {
+                    
+                    favoriteButton.setTitle(FavoriteType.remove.rawValue, for: .normal)
+                    
+                    break
+                }
             }
         }
         
     }
     
-    func toggleFavoriteButton(with favoriteButton: UIButton) {
+    // Use for when user tap add/remove favorite
+    func toggleFavoriteButton(with favoriteButton: UIButton, completion: @escaping (Error?) -> Void) {
         
+        // Save data
         if favoriteButton.currentTitle == FavoriteType.add.rawValue {
             
-            addToFavoriteInLS()
+            if !didLogin {
+                
+                addToFavoriteInLS()
+                
+            } else {
+                
+                addToFavoriteInFB { error in
+                    
+                    completion(error)
+                }
+            }
             
+        // Remove data
         } else {
             
-            removeFavoriteFromLS()
+            if !didLogin {
+                
+                removeFavoriteFromLS()
+                
+            } else {
+                
+                removeFavoriteFromFB()
+            }
         }
         
         favoriteButton.setTitle(
@@ -112,36 +208,20 @@ class AdoptDetailViewModel {
         )
     }
     
-    func fetchFavoritePetFromLS(completion: (Error?) -> Void ) {
-        
-        StorageManager.shared.fetchPet { result in
-            
-            switch result {
-                
-            case .success(let lsPets):
-                
-                self.setPets(with: lsPets)
-                self.favoritePetsFromLS = lsPets
-                
-            case .failure(let error):
-                
-                completion(error)
-            }
-        }
-        
-    }
     
     
     
-    private func convertLsPetsToViewModels(from lsPets: [LSPet]) -> [FavoritePetViewModel] {
+    // MARK: - Private functions
+    // Local storage
+    private func convertLsPetsToViewModels(from lsPets: [LSPet]) -> [FavoriteLSPetViewModel] {
         
         //        let pets = convertLsPetsToPets(from: lsPets)
         
-        var viewModels = [FavoritePetViewModel]()
+        var viewModels = [FavoriteLSPetViewModel]()
         
         for lsPet in lsPets {
             
-            let viewModel = FavoritePetViewModel(model: lsPet)
+            let viewModel = FavoriteLSPetViewModel(model: lsPet)
             
             viewModels.append(viewModel)
         }
@@ -151,7 +231,26 @@ class AdoptDetailViewModel {
     
     private func setPets(with lsPets: [LSPet]) {
         
-        favoritePetViewModels.value = convertLsPetsToViewModels(from: lsPets)
+        favoriteLSPetViewModels.value = convertLsPetsToViewModels(from: lsPets)
+    }
+    
+    // Firebase
+    private func convertPetsToViewModels(from pets: [Pet]) -> [PetViewModel] {
+        
+        var viewModels = [PetViewModel]()
+        
+        for pet in pets {
+            
+            let viewModel = PetViewModel(model: pet)
+            
+            viewModels.append(viewModel)
+        }
+        return viewModels
+    }
+    
+    private func setPets(_ pets: [Pet]) {
+        
+        favoritePetViewModels.value = convertPetsToViewModels(from: pets)
     }
     
 }
