346633ff1928dbdcf525f8cd1e96dfdbb06aa8ba
Wayne <h88377@gmail.com>
2022-04-12 16:03:46 +0800

    Modify FindPetSocietyCell layout.

--- FindYourOnlys/View/FindPetSociety/FindPetSocietyTableViewCell.swift
+++ FindYourOnlys/View/FindPetSociety/FindPetSocietyTableViewCell.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class FindPetSocietyTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var userButton: UIButton!
+    
+    @IBOutlet weak var userIdLabel: UILabel!
+    
+    @IBOutlet weak var postedImageView: UIImageView!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    
+    @IBOutlet weak var leaveCommentButton: UIButton!
+    
+    @IBOutlet weak var shareButton: UIButton!
+    
+    @IBOutlet weak var likeCountLabel: UILabel!
+    
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
