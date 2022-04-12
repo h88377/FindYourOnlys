27e494b289529399ec60937e35a1482dec5ffa8e
Wayne <h88377@gmail.com>
2022-04-12 22:00:48 +0800

    Create PushCells layout.

---
+++ FindYourOnlys/View/FindPetSociety/PublishContentCell.swift
@@ -0,0 +1,22 @@
+//
+//  PublishMediaCell.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/12.
+//
+
+import UIKit
+
+class PublishContentCell: UITableViewCell {
+    
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    @IBAction func openCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func openGallery(_ sender: UIButton) {
+    }
+    
+}
---
+++ FindYourOnlys/View/FindPetSociety/PublishKindCell.swift
@@ -0,0 +1,16 @@
+//
+//  PublishKindCell.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/12.
+//
+
+import UIKit
+
+class PublishKindCell: UITableViewCell {
+
+    @IBOutlet weak var kindLabel: UILabel!
+    
+    @IBOutlet weak var kindStackView: UIStackView!
+    
+}
