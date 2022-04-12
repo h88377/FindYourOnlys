13896a6fae622215591ce452ddb9ed8953ad0a10
Wayne <h88377@gmail.com>
2022-04-12 19:45:13 +0800

    Create FindPetSocietyTableViewCell layout.

---
+++ FindYourOnlys/Controller/FindPetSociety/PublishViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PublishViewController.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/12.
+//
+
+import UIKit
+
+class PublishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
