7eaf72386feb3fdfd41e42493a850895d5c5ea68
Wayne <h88377@gmail.com>
2022-04-12 15:10:59 +0800

    Create HomeTabBarVC to populate controllers.

--- FindYourOnlys/Controller/Adopt/AdoptListViewController.swift
+++ FindYourOnlys/Controller/Adopt/AdoptListViewController.swift
@@ -99,10 +99,11 @@ extension AdoptListViewController: UICollectionViewDataSource, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        let storyboard = UIStoryboard.adopt
+        
         guard
-            let adoptDetaiVC = storyboard?.instantiateViewController(withIdentifier: AdoptDetailViewController.identifier) as? AdoptDetailViewController,
-            let adoptFavoriteVC = storyboard?.instantiateViewController(withIdentifier: AdoptFavoriteViewController.identifier) as? AdoptFavoriteViewController
-                
+            let adoptDetaiVC = storyboard.instantiateViewController(withIdentifier: AdoptDetailViewController.identifier) as? AdoptDetailViewController,
+            let adoptFavoriteVC = storyboard.instantiateViewController(withIdentifier: AdoptFavoriteViewController.identifier) as? AdoptFavoriteViewController
                 
         else { return }
         
