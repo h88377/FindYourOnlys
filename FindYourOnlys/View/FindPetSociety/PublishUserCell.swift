731f986924c87920bda7081a402f2f7357c907db
Wayne <h88377@gmail.com>
2022-04-12 22:00:48 +0800

    Create PushCells layout.

---
+++ FindYourOnlys/View/FindPetSociety/PublishUserCell.swift
@@ -0,0 +1,16 @@
+//
+//  PublishUserCell.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/12.
+//
+
+import UIKit
+
+class PublishUserCell: UITableViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var userNickNameLabel: UILabel!
+   
+}
