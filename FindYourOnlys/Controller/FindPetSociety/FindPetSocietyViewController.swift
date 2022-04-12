aae03c5206d162b482eedec01227cdde7bc94855
Wayne <h88377@gmail.com>
2022-04-12 15:46:36 +0800

    Finish FindPetSocietyTableViewCell layout.

---
+++ FindYourOnlys/Controller/FindPetSociety/FindPetSocietyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindPetSocietyViewController.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/12.
+//
+
+import UIKit
+
+class FindPetSocietyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
