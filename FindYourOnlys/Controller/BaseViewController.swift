9674eb70429a02899159adfc11b8df1ba9c194dc
Wayne <h88377@gmail.com>
2022-04-14 18:04:09 +0800

    Create BaseUIViewcontroller provide default value and fix real device no image issue.

---
+++ FindYourOnlys/Controller/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/14.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
