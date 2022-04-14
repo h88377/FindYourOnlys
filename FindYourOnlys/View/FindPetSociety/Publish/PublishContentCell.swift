46848d0f7a4503263e2bee16ebb229e361d0e4f3
Wayne <h88377@gmail.com>
2022-04-14 10:44:21 +0800

    Pass data from PublishCell to PublishViewModel and install IQKeyboardManagerSwift framework.

--- FindYourOnlys/View/FindPetSociety/Publish/PublishContentCell.swift
+++ FindYourOnlys/View/FindPetSociety/Publish/PublishContentCell.swift
@@ -11,7 +11,29 @@ class PublishContentCell: PublishBasicCell {
     
     @IBOutlet weak var contentImageView: UIImageView!
     
-    @IBOutlet weak var contentTextView: ContentInsetTextField!
+    @IBOutlet weak var contentTextView: UITextView! {
+        
+        didSet {
+            
+            contentTextView.delegate = self
+            
+            contentTextView.text = "Here is your story"
+            
+            contentTextView.textColor = UIColor.systemGray3
+            
+            contentTextView.layer.borderWidth = 1
+            
+            contentTextView.layer.borderColor = UIColor.systemGray5.cgColor
+            
+            contentTextView.inputAccessoryView = customDoneToolBar()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentTextView.layer.cornerRadius = 5
+    }
     
     @IBAction func openCamera(_ sender: UIButton) {
         
@@ -27,6 +49,50 @@ class PublishContentCell: PublishBasicCell {
         
         contentImageView.image = image
     }
+    
+    func customDoneToolBar() -> UIToolbar {
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        
+        toolbar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        
+        toolbar.setItems([flexibleSpace, doneBtn], animated: true)
+        
+        return toolbar
+    }
+    
+    @objc func donePressed() {
+
+        contentTextView.endEditing(true)
+    }
 }
 
-
+// MARK: - UITextViewDelegate
+extension PublishContentCell: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        if textView.textColor == UIColor.systemGray3 {
+            
+            textView.text = nil
+            
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        if textView.text.isEmpty {
+            
+            textView.text = "Here is your story"
+            
+            textView.textColor = UIColor.systemGray3
+        }
+        
+        delegate?.didChangeContent(self, with: textView.text)
+    }
+}
