123e494dfc03cebc865ab7c51f566571753f24f4
Wayne <h88377@gmail.com>
2022-04-17 11:59:41 +0800

    Create Add/SearchFriendVC layout.

---
+++ FindYourOnlys/Controller/FindPetSociety/Add Friend/SearchFriendViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchFriendViewController.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/17.
+//
+
+import UIKit
+
+class SearchFriendViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
