353161f7307d91e887eac5ceff4ef6bbceaa41e7
Wayne <h88377@gmail.com>
2022-04-19 20:39:25 +0800

    Display navigation route on AdoptDirectionVC and present on AdoptPetLocationVC.

---
+++ FindYourOnlys/Extension/MKMapView+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  MKMapView+Extension.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/19.
+//
+
+import MapKit
+
+extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 50000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
---
+++ FindYourOnlys/ViewModel/Adopt/DirectionViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DirectionViewModel.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/19.
+//
+
+import MapKit
+
+class DirectionViewModel {
+    
+    var direction: Direction
+    
+    init(model: Direction) {
+        
+        self.direction = model
+    }
+}
