a81fc326bb9ff45cc9c5b7ed8f37c8438f3f906e
Wayne <h88377@gmail.com>
2022-04-19 15:17:53 +0800

    Get current position and place on map in PetLocationVC.

---
+++ FindYourOnlys/Model/MapAnnotation.swift
@@ -0,0 +1,32 @@
+//
+//  Placemark.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/19.
+//
+
+import Foundation
+import CoreLocation
+import MapKit
+
+class MapAnnotation: NSObject, MKAnnotation {
+    
+    let title: String?
+    
+    let subtitle: String?
+    
+    let location: String
+    
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, subtitle: String, location: String, coordinate: CLLocationCoordinate2D) {
+        
+        self.title = title
+        
+        self.subtitle = subtitle
+        
+        self.location = location
+        
+        self.coordinate = coordinate
+    }
+}
