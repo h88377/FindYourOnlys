9064d5f00451bd4773aef4a0339c39d73d15f1eb
Wayne <h88377@gmail.com>
2022-04-19 14:05:25 +0800

    Get destination position on map when user click check location in AdoptDetailVC.

---
+++ FindYourOnlys/ViewModel/Adopt/PetLocationViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  PetLocationViewModel.swift
+//  FindYourOnlys
+//
+//  Created by 鄭昭韋 on 2022/4/19.
+//
+
+import Foundation
+import MapKit
+
+class PetLocationViewModel {
+    
+    var petViewModel = Box(
+        PetViewModel(
+            model: Pet(
+                id: 0, location: "", kind: "",
+                sex: "", bodyType: "", color: "",
+                age: "", sterilization: "", bacterin: "",
+                foundPlace: "", status: "", remark: "",
+                openDate: "", closedDate: "", updatedDate: "",
+                createdDate: "", photoURLString: "", address: "",
+                telephone: "", variety: ""
+            )
+        )
+    )
+    
+    func convertAddress(completion: @escaping (CLLocation) -> Void) {
+        
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.geocodeAddressString("\(petViewModel.value.pet.address)") { placemarks, error in
+            
+            guard
+                let placemarks = placemarks,
+                
+                let location = placemarks.first?.location
+                    
+            else { return }
+            
+            completion(location)
+        }
+    }
+    
+//    func getInitialLocation(mapView: MKMapView) {
+//        
+//        convertAddress { location in
+//            
+//            let initialLocation = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//            
+//            mapView.centerToLocation(initialLocation)
+//        }
+//        
+//        
+//        
+//        mapView.centerToLocation(initialLocation)
+//        
+//    }
+    
+}
