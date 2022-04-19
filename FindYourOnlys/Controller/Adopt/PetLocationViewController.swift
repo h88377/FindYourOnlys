20f6b0d7f7451735208ccd2729842a8f7750766f
Wayne <h88377@gmail.com>
2022-04-19 15:17:53 +0800

    Get current position and place on map in PetLocationVC.

--- FindYourOnlys/Controller/Adopt/PetLocationViewController.swift
+++ FindYourOnlys/Controller/Adopt/PetLocationViewController.swift
@@ -15,43 +15,159 @@ class PetLocationViewController: BaseViewController {
     
     let viewModel = PetLocationViewModel()
     
+    private let locationManager = CLLocationManager()
+    
+    //    var placemark: Placemark {
+    //
+    //
+    //    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        
         viewModel.convertAddress { [weak self] location in
             
-            self?.mapView.centerToLocation(location)
+            guard
+                let self = self else { return }
+            
+            self.mapView.centerToLocation(location)
+            
+            let pet = self.viewModel.petViewModel.value.pet
+            
+            let mapAnnotation = MapAnnotation(
+                title: pet.kind, subtitle: pet.address, location: pet.location,
+                coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            )
+            
+            self.mapView.addAnnotation(mapAnnotation)
+            
+            
         }
         
-
-//        let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
-//        let region = MKCoordinateRegion(
-//          center: oahuCenter.coordinate,
-//          latitudinalMeters: 50000,
-//          longitudinalMeters: 60000)
-//        mapView.setCameraBoundary(
-//          MKMapView.CameraBoundary(coordinateRegion: region),
-//          animated: true)
-//
-//        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-//        mapView.setCameraZoomRange(zoomRange, animated: true)
-
-
     }
     
-
-
+    private func attemptLocationAccess() {
+        
+        guard
+            CLLocationManager.locationServicesEnabled()
+                
+        else {
+            
+            return
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            
+            locationManager.requestWhenInUseAuthorization()
+            
+        } else {
+            
+            locationManager.requestLocation()
+        }
+    }
+    
+    func addAnnotation(with location: CLLocation) {
+        
+        
+        
+    }
+    
+    @IBAction func navigate(_ sender: UIButton) {
+        
+        attemptLocationAccess()
+    }
+    
 }
 
 private extension MKMapView {
-  func centerToLocation(
-    _ location: CLLocation,
-    regionRadius: CLLocationDistance = 50000
-  ) {
-    let coordinateRegion = MKCoordinateRegion(
-      center: location.coordinate,
-      latitudinalMeters: regionRadius,
-      longitudinalMeters: regionRadius)
-    setRegion(coordinateRegion, animated: true)
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 50000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension PetLocationViewController: CLLocationManagerDelegate {
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        // 1
+        guard
+            status == .authorizedWhenInUse
+                
+        else {
+            
+            return
+        }
+        manager.requestLocation()
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        
+        guard
+            let firstLocation = locations.first
+                
+        else {
+            
+            return
+        }
+        
+        // TODO: Configure MKLocalSearchCompleter here...
+        
+        // 2
+              CLGeocoder().reverseGeocodeLocation(firstLocation) { [weak self] places, _ in
+        // 3
+                guard
+                  let firstPlace = places?.first,
+                  let self = self
+                    
+                  else { return }
+                  
+                  let pet = self.viewModel.petViewModel.value.pet
+                  
+                  let currentAnnotation = MapAnnotation(
+                    title: "現在位置", subtitle: "\(firstPlace.abbreviation)", location: "\(firstPlace)",
+                      coordinate: CLLocationCoordinate2D(latitude: firstLocation.coordinate.latitude, longitude: firstLocation.coordinate.longitude)
+                  )
+                  
+                  self.mapView.addAnnotation(currentAnnotation)
+              }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+      print("Error requesting location: \(error.localizedDescription)")
+    }
+    
+}
+
+extension CLPlacemark {
+  var abbreviation: String {
+    if let name = self.name {
+      return name
+    }
+
+    if let interestingPlace = areasOfInterest?.first {
+      return interestingPlace
+    }
+
+    return [subThoroughfare, thoroughfare].compactMap { $0 }.joined(separator: " ")
   }
 }
