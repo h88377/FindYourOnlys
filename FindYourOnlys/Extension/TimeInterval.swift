0de898641d9cfa2006fa509c73298e2277c4222e
Wayne <h88377@gmail.com>
2022-04-20 10:18:00 +0800

    Optimize User experience in AdoptPetLocationVC navigation feature.

--- FindYourOnlys/Extension/TimeInterval.swift
+++ FindYourOnlys/Extension/TimeInterval.swift
@@ -9,14 +9,14 @@ import Foundation
 
 extension TimeInterval {
     
-  var formatted: String {
-      
-    let formatter = DateComponentsFormatter()
-      
-    formatter.unitsStyle = .full
-      
-    formatter.allowedUnits = [.hour, .minute]
-
-    return formatter.string(from: self) ?? ""
-  }
+    var formatted: String {
+        
+        let formatter = DateComponentsFormatter()
+        
+        formatter.unitsStyle = .full
+        
+        formatter.allowedUnits = [.hour, .minute]
+        
+        return formatter.string(from: self) ?? ""
+    }
 }
