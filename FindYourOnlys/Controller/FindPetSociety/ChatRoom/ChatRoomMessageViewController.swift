9cfe71ace065ba5132dccb7bc38d0081ab1edb9b
Wayne <h88377@gmail.com>
2022-04-22 16:49:34 +0800

    Move showAlertWindow func in BaseVC and add filter isValid logic.

--- FindYourOnlys/Controller/FindPetSociety/ChatRoom/ChatRoomMessageViewController.swift
+++ FindYourOnlys/Controller/FindPetSociety/ChatRoom/ChatRoomMessageViewController.swift
@@ -152,17 +152,6 @@ class ChatRoomMessageViewController: BaseViewController {
         }
     }
     
-    func showAlertWindow(title: String, message: String) {
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "OK", style: .default)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true)
-    }
-    
     override func setupTableView() {
         
         tableView.registerCellWithIdentifier(identifier: ChatRoomMessageCell.identifier)
