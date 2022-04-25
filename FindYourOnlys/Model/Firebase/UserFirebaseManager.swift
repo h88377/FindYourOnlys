57fd821fa5124f57ba1cf651411ea471b4e8f061
Wayne <h88377@gmail.com>
2022-04-25 13:26:17 +0800

    Clean code

--- FindYourOnlys/Model/Firebase/UserFirebaseManager.swift
+++ FindYourOnlys/Model/Firebase/UserFirebaseManager.swift
@@ -17,26 +17,26 @@ class UserFirebaseManager {
     
     // swiftlint:disable line_length
     // Wayne
-    var currentUser: String { return "123" }
-
-    var currentUserImageURL: String { return "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123.jpeg?alt=media&token=fdac6ab2-47e1-4f9a-b5f2-20c464e7f911" }
-    var currentUserInfo: User {
-
-        return User(id: "123", nickName: "Wayne", email: "123@email", imageURLString: "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123.jpeg?alt=media&token=fdac6ab2-47e1-4f9a-b5f2-20c464e7f911",
-                    friends: ["321", "456", "654"], limitedUsers: ["444"])
-    }
+//    var currentUser: String { return "123" }
+//
+//    var currentUserImageURL: String { return "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123.jpeg?alt=media&token=fdac6ab2-47e1-4f9a-b5f2-20c464e7f911" }
+//    var currentUserInfo: User {
+//
+//        return User(id: "123", nickName: "Wayne", email: "123@email", imageURLString: "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123.jpeg?alt=media&token=fdac6ab2-47e1-4f9a-b5f2-20c464e7f911",
+//                    friends: ["321", "456", "654"], limitedUsers: ["444"])
+//    }
     
     // Luke
     
-//    var currentUser: String { return "321" }
-//
-//    var currentUserImageURL: String { return "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123with%20time%201649930593.570539.jpeg?alt=media&token=85c08303-a395-49c8-a45e-6650258037f2" }
-//
-//    var currentUserInfo: User {
-//
-//        return User(id: "321", nickName: "Luke", email: "321@email", imageURLString: "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123with%20time%201649930593.570539.jpeg?alt=media&token=85c08303-a395-49c8-a45e-6650258037f2",
-//                    friends: ["123"], limitedUsers: ["444"])
-//    }
+    var currentUser: String { return "321" }
+
+    var currentUserImageURL: String { return "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123with%20time%201649930593.570539.jpeg?alt=media&token=85c08303-a395-49c8-a45e-6650258037f2" }
+
+    var currentUserInfo: User {
+
+        return User(id: "321", nickName: "Luke", email: "321@email", imageURLString: "https://firebasestorage.googleapis.com:443/v0/b/findyouronlys.appspot.com/o/images%2F123with%20time%201649930593.570539.jpeg?alt=media&token=85c08303-a395-49c8-a45e-6650258037f2",
+                    friends: ["123"], limitedUsers: ["444"])
+    }
     
     func fetchUser(completion: @escaping (Result<[User], Error>) -> Void) {
         
