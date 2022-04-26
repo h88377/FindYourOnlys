74c37e7125e126c97bca4b2294906fe85c247c4d
Wayne <h88377@gmail.com>
2022-04-26 17:58:45 +0800

    Get current user object after user sign in with Firebase.

--- FindYourOnlys/ViewModel/Auth/SignInViewModel.swift
+++ FindYourOnlys/ViewModel/Auth/SignInViewModel.swift
@@ -15,19 +15,38 @@ class SignInViewModel {
     
     func signIn(withEmail email: String, password: String) {
         
-        UserFirebaseManager.shared.signIn(withEmail: email, password: password) { [weak self] error in
+        UserFirebaseManager.shared.signIn(withEmail: email, password: password) { [weak self] result in
             
-            guard
-                error == nil
-                    
-            else {
+            switch result {
                 
-                self?.errorViewModel.value = ErrorViewModel(model: error!)
+            case .success(let registeredUserId):
                 
-                return
+                UserFirebaseManager.shared.fetchUser { [weak self] result in
+
+                    switch result {
+
+                    case .success(let users):
+
+                        for user in users where user.id == registeredUserId {
+
+                            UserFirebaseManager.shared.currentFBUserInfo = user
+                            
+                            self?.dismissHandler?()
+                            
+                            break
+                        }
+
+                    case .failure(let error):
+
+                        self?.errorViewModel.value = ErrorViewModel(model: error)
+                    }
+                }
+                
+            case .failure(let error):
+                
+                self?.errorViewModel.value = ErrorViewModel(model: error)
             }
             
-            self?.dismissHandler?()
         }
     }
     
