6517335355bc897461590fac1c86ee68f83eb95c
Wayne <h88377@gmail.com>
2022-04-27 19:35:03 +0800

    Finish commentCell layout and clean code of articleCell.

--- FindYourOnlys/View/FindPetSociety/Article/CommentCell.swift
+++ FindYourOnlys/View/FindPetSociety/Article/CommentCell.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class CommentCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var nickNameLabel: UILabel!
     
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    @IBOutlet weak var createdTimeLabel: UILabel!
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        userImageView.layer.cornerRadius = userImageView.frame.height / 2
+    }
 }
